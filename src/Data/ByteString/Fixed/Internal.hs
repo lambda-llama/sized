3c772d0964395505ad86d7ff7af3e86a38ea08c8
Fedor Gogolev <knsd@knsd.net>
2013-04-03 01:09:34 +0400

    Initial commit

---
+++ src/Data/ByteString/Fixed/Internal.hs
@@ -0,0 +1,70 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE MultiWayIf #-}
+{-# LANGUAGE DataKinds #-}
+
+module Data.ByteString.Fixed.Internal
+    ( FixedByteString(..)
+    , fromByteString
+    , toByteString
+    ) where
+
+import GHC.ForeignPtr (ForeignPtr, mallocPlainForeignPtrBytes)
+import GHC.TypeLits (Nat)
+
+import Control.DeepSeq
+import Data.ByteString (ByteString)
+import Data.ByteString.Unsafe (unsafeUseAsCStringLen, unsafePackCStringLen)
+import Data.Word (Word8)
+import Foreign (castPtr, withForeignPtr)
+import System.IO.Unsafe (unsafeDupablePerformIO)
+import Unsafe.Coerce (unsafeCoerce)
+
+import Data.ByteString.Fixed.Internal.Foreign (memcpy, memcmp)
+
+data FixedByteString (size :: Nat) = FixedByteString {-# UNPACK #-} !(ForeignPtr Word8)
+    deriving (Show)
+
+data NatProxy (nat :: Nat)
+
+class NatReflection (nat :: Nat) where
+    nat :: NatProxy nat -> Int
+
+instance NFData (FixedByteString size)
+
+instance NatReflection a => Eq (FixedByteString a) where
+    FixedByteString fp1 == FixedByteString fp2 = unsafeDupablePerformIO $ do
+        withForeignPtr fp1 $ \p1 -> withForeignPtr fp2 $ \p2 -> do
+            cmp <- memcmp p1 p2 size
+            return $ cmp == 0
+      where
+        size = fromIntegral $ nat (undefined :: NatProxy a)
+
+instance NatReflection a => Ord (FixedByteString a) where
+    FixedByteString fp1 `compare` FixedByteString fp2 = unsafeDupablePerformIO $ do
+        withForeignPtr fp1 $ \p1 -> withForeignPtr fp2 $ \p2 -> do
+            memcmp p1 p2 size >>= \c -> return $ if
+              | c == 0 -> EQ
+              | c <  0 -> LT
+              | c >  0 -> GT
+      where
+        size = fromIntegral $ nat (undefined :: NatProxy a)
+
+fromByteString :: forall a. NatReflection a => ByteString -> Maybe (FixedByteString a)
+fromByteString b = unsafeCoerce $ unsafeDupablePerformIO $
+    unsafeUseAsCStringLen b $ \(p, size) -> do
+        let expectedSize = nat (undefined :: NatProxy a)
+        case expectedSize == size of
+            True -> do
+                fp <- mallocPlainForeignPtrBytes size
+                withForeignPtr fp $ \p' -> do
+                    memcpy p' (castPtr p) $ fromIntegral size
+                    return $! Just $ FixedByteString fp
+            False -> return $ Nothing
+{-# INLINE fromByteString #-}
+
+toByteString :: forall a. NatReflection a => FixedByteString a -> ByteString
+toByteString (FixedByteString fp) = unsafeDupablePerformIO $ withForeignPtr fp $ \p -> do
+    let size = nat (undefined :: NatProxy a)
+    unsafePackCStringLen (castPtr p, fromIntegral size)
---
+++ src/Data/ByteString/Fixed/Internal/Foreign.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE ForeignFunctionInterface #-}
+
+module Data.ByteString.Fixed.Internal.Foreign
+    ( memcpy
+    , memcmp
+    ) where
+
+import Data.Word (Word8)
+import Foreign (Ptr)
+import Foreign.C.Types (CSize(..), CInt(..))
+
+foreign import ccall unsafe "string.h memcpy" memcpy :: Ptr Word8 -> Ptr Word8 -> CSize -> IO ()
+foreign import ccall unsafe "string.h memcmp" memcmp :: Ptr Word8 -> Ptr Word8 -> CSize -> IO CInt
