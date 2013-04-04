dfd00cba8a5645ab0e7432b70cecb4d9515be369
Fedor Gogolev <knsd@knsd.net>
2013-04-04 09:27:41 +0400

    A little better code

--- src/Data/Sized/ByteString.hs
+++ src/Data/Sized/ByteString.hs
@@ -16,12 +16,13 @@ import GHC.IO (IO(IO))
 
 import Control.DeepSeq (NFData)
 import Data.ByteString (ByteString)
-import Data.ByteString.Unsafe (unsafeUseAsCStringLen, unsafePackCStringLen)
+import Data.ByteString.Unsafe (unsafeUseAsCString, unsafePackCStringLen)
 import Data.Word (Word8)
-import Foreign (castPtr, withForeignPtr)
+import Foreign (Ptr, castPtr, withForeignPtr)
 import Foreign.Storable (Storable(..))
 import System.IO.Unsafe (unsafeDupablePerformIO)
 import Unsafe.Coerce (unsafeCoerce)
+import qualified Data.ByteString as ByteString
 
 import Data.Sized.Nat (Nat, NatProxy, NatReflection(nat))
 import Data.Sized.Foreign (memcpy, memcmp)
@@ -52,26 +53,31 @@ instance NatReflection a => Ord (SizedByteString a) where
 instance NatReflection a => Storable (SizedByteString a) where
     sizeOf _ = nat (undefined :: NatProxy a)
     alignment _ = alignment (undefined :: Word8)
-    peek ptr = do
-        let size = nat (undefined :: NatProxy a)
-        fp <- mallocPlainForeignPtrBytes size
-        withForeignPtr fp $ \p' -> do
-            memcpy p' (castPtr ptr) $ fromIntegral size
-            return $ SizedByteString fp
+    peek = unsafeFromPtr
     poke ptr f@(SizedByteString fp) = withForeignPtr fp $ \p ->
         memcpy (castPtr ptr) p $ fromIntegral $ sizeOf f
 
+unsafeFromPtr :: forall a b. NatReflection a => Ptr b -> IO (SizedByteString a)
+unsafeFromPtr p = do
+    let size = nat (undefined :: NatProxy a)
+    fp <- mallocPlainForeignPtrBytes size
+    withForeignPtr fp $ \p' -> do
+        memcpy p' (castPtr p) $ fromIntegral size
+        return $! SizedByteString fp
+{-# INLINE unsafeFromPtr #-}
+
+unsafeFromByteString :: forall a. NatReflection a => ByteString -> SizedByteString a
+unsafeFromByteString b = unsafeCoerce $ unsafeDupablePerformIO $
+    unsafeUseAsCString b $ \p -> unsafeFromPtr p :: IO (SizedByteString a)
+{-# INLINE unsafeFromByteString #-}
+
 fromByteString :: forall a. NatReflection a => ByteString -> Maybe (SizedByteString a)
-fromByteString b = unsafeCoerce $ unsafeDupablePerformIO $
-    unsafeUseAsCStringLen b $ \(p, size) -> do
-        let expectedSize = nat (undefined :: NatProxy a)
-        case expectedSize == size of
-            True -> do
-                fp <- mallocPlainForeignPtrBytes size
-                withForeignPtr fp $ \p' -> do
-                    memcpy p' (castPtr p) $ fromIntegral size
-                    return $! Just $ SizedByteString fp
-            False -> return $ Nothing
+fromByteString b
+    | bsize == expected = Just $ unsafeFromByteString b
+    | otherwise         = Nothing
+  where
+    bsize = ByteString.length b
+    expected = nat (undefined :: NatProxy a)
 {-# INLINE fromByteString #-}
 
 toByteString :: forall a. NatReflection a => SizedByteString a -> ByteString
