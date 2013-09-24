420a49a4bb514e91ebd2ec5ca3494b65a1697f50
Fedor Gogolev <knsd@knsd.net>
2013-09-24 16:05:40 +0400

    Benchmark singleton function

--- benchmarks/BenchmarksAgainstByteString.hs
+++ benchmarks/BenchmarksAgainstByteString.hs
@@ -3,10 +3,12 @@ module Main (main) where
 import Prelude hiding (replicate)
 import Data.ByteString.Char8 (ByteString, replicate, snoc)
 import Data.Maybe (fromJust)
+import qualified Data.ByteString as ByteString
 
-import Criterion.Main (defaultMain, bench, nf, whnf)
+import Criterion.Main (defaultMain, bench, whnf)
 
 import Data.Sized.ByteString (SizedByteString, fromByteString)
+import qualified Data.Sized.ByteString as SizedByteString
 
 main :: IO ()
 main = do
@@ -14,8 +16,10 @@ main = do
     let bs2 :: ByteString = snoc (replicate 31 'a') 'b'
     let fbs1 :: SizedByteString 32 = fromJust $ fromByteString bs1
     let fbs2 :: SizedByteString 32 = fromJust $ fromByteString bs2
-    defaultMain [ bench "eq bytestring" $ nf ( == bs1) bs2
-                , bench "eq fixed" $ nf ( == fbs1) fbs2
+    defaultMain [ bench "eq bytestring" $ whnf ( == bs1) bs2
+                , bench "eq fixed" $ whnf ( == fbs1) fbs2
                 , bench "ord bytestring" $ whnf (`compare` bs1) bs2
                 , bench "ord fixed" $ whnf (`compare` fbs1) fbs2
+                , bench "singleton bytestring" $ whnf (ByteString.singleton) 0
+                , bench "singleton fixed" $ whnf (SizedByteString.singleton) 0
                 ]
