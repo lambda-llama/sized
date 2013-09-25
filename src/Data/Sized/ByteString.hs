4d0ba4f06c339f0945df0e2c5d189a447291a881
Fedor Gogolev <knsd@knsd.net>
2013-09-25 11:47:58 +0400

    Use SingI instead of NatReflection

--- src/Data/Sized/ByteString.hs
+++ src/Data/Sized/ByteString.hs
@@ -7,7 +7,7 @@
 
 module Data.Sized.ByteString
     ( SizedByteString(..)
-    , NatReflection(..), Proxy
+    , SingI(..)
 
     , fromByteString
     , toByteString
@@ -40,9 +40,6 @@ import System.IO.Unsafe (unsafeDupablePerformIO)
 import Unsafe.Coerce (unsafeCoerce)
 import qualified Data.ByteString as ByteString
 
-import Data.Proxy (Proxy(..))
-
-import Data.Sized.Nat (NatReflection(nat))
 import Data.Sized.Foreign (memcpy, memcmp)
 
 data SizedByteString (size :: Nat) = SizedByteString {-# UNPACK #-} !(ForeignPtr Word8)
@@ -50,15 +47,15 @@ data SizedByteString (size :: Nat) = SizedByteString {-# UNPACK #-} !(ForeignPtr
 
 instance NFData (SizedByteString size)
 
-instance NatReflection a => Eq (SizedByteString a) where
+instance SingI a => Eq (SizedByteString a) where
     SizedByteString fp1 == SizedByteString fp2 = inlinePerformIO $ do
         withForeignPtr fp1 $ \p1 -> withForeignPtr fp2 $ \p2 -> do
             cmp <- memcmp p1 p2 size
             return $ cmp == 0
       where
-        size = fromIntegral $ nat (Proxy :: Proxy a)
+        size = fromIntegral $ rep (sing :: Sing a)
 
-instance NatReflection a => Ord (SizedByteString a) where
+instance SingI a => Ord (SizedByteString a) where
     SizedByteString fp1 `compare` SizedByteString fp2 = inlinePerformIO $ do
         withForeignPtr fp1 $ \p1 -> withForeignPtr fp2 $ \p2 -> do
             memcmp p1 p2 size >>= \c -> return $ if
@@ -66,10 +63,10 @@ instance NatReflection a => Ord (SizedByteString a) where
                 | c <  0 -> LT
                 | c >  0 -> GT
       where
-        size = fromIntegral $ nat (Proxy :: Proxy a)
+        size = fromIntegral $ rep (sing :: Sing a)
 
-instance NatReflection a => Storable (SizedByteString a) where
-    sizeOf _ = nat (Proxy :: Proxy a)
+instance SingI a => Storable (SizedByteString a) where
+    sizeOf _ = rep (sing :: Sing a)
     alignment _ = alignment (undefined :: Word8)
     peek = unsafeFromPtr
     poke ptr f@(SizedByteString fp) = withForeignPtr fp $ \p ->
@@ -79,35 +76,39 @@ inlinePerformIO :: IO a -> a
 inlinePerformIO (IO m) = case m realWorld# of (# _, r #) -> r
 {-# INLINE inlinePerformIO #-}
 
-unsafeCreate :: forall a. NatReflection a => (Ptr Word8 -> IO ()) -> IO (SizedByteString a)
+rep :: Sing (a :: Nat) -> Int
+rep = fromInteger . fromSing
+{-# INLINE rep #-}
+
+unsafeCreate :: forall a. SingI a => (Ptr Word8 -> IO ()) -> IO (SizedByteString a)
 unsafeCreate f = do
-    let size = nat (Proxy :: Proxy a)
+    let size = rep (sing :: Sing a)
     fp <- mallocPlainForeignPtrBytes size
     withForeignPtr fp f
     return $! SizedByteString fp
 {-# INLINE unsafeCreate #-}
 
-unsafeFromPtr :: forall a b. NatReflection a => Ptr b -> IO (SizedByteString a)
+unsafeFromPtr :: forall a b. SingI a => Ptr b -> IO (SizedByteString a)
 unsafeFromPtr p = do
-    let size = nat (Proxy :: Proxy a)
+    let size = rep (sing :: Sing a)
     unsafeCreate $ \p' -> memcpy p' (castPtr p) $ fromIntegral size
 {-# INLINE unsafeFromPtr #-}
 
-unsafeFromByteString :: forall a. NatReflection a => ByteString -> SizedByteString a
+unsafeFromByteString :: forall a. SingI a => ByteString -> SizedByteString a
 unsafeFromByteString b = unsafeCoerce $ unsafeDupablePerformIO $
     unsafeUseAsCString b $ \p -> unsafeFromPtr p :: IO (SizedByteString a)
 {-# INLINE unsafeFromByteString #-}
 
-fromByteString :: forall a. NatReflection a => ByteString -> Maybe (SizedByteString a)
+fromByteString :: forall a. SingI a => ByteString -> Maybe (SizedByteString a)
 fromByteString b = let bsize = ByteString.length b
-                       expected = nat (Proxy :: Proxy a) in if
+                       expected = rep (sing :: Sing a) in if
     | bsize == expected -> Just $ unsafeFromByteString b
     | otherwise         -> Nothing
 {-# INLINE fromByteString #-}
 
-toByteString :: forall a. NatReflection a => SizedByteString a -> ByteString
+toByteString :: forall a. SingI a => SizedByteString a -> ByteString
 toByteString (SizedByteString fp) = unsafeDupablePerformIO $ withForeignPtr fp $ \p -> do
-    let size = nat (Proxy :: Proxy a)
+    let size = rep (sing :: Sing a)
     unsafePackCStringLen (castPtr p, fromIntegral size)
 {-# INLINE toByteString #-}
 
@@ -119,32 +120,32 @@ singleton :: Word8 -> SizedByteString 1
 singleton c = unsafeDupablePerformIO $ unsafeCreate $ \p -> poke p c
 {-# INLINE singleton #-}
 
-null :: forall a. NatReflection a => SizedByteString a -> Bool
-null = const $ nat (Proxy :: Proxy a) == 0
+null :: forall a. SingI a => SizedByteString a -> Bool
+null = const $ rep (sing :: Sing a) == 0
 {-# INLINE null #-}
 
-length :: forall a. NatReflection a => SizedByteString a -> Int
-length = const $ nat (Proxy :: Proxy a)
+length :: forall a. SingI a => SizedByteString a -> Int
+length = const $ rep (sing :: Sing a)
 {-# INLINE length #-}
 
-cons :: forall a b. (b ~ (a + 1), NatReflection a, NatReflection b) => Word8 -> SizedByteString a -> SizedByteString b
+cons :: forall a b. (b ~ (a + 1), SingI a, SingI b) => Word8 -> SizedByteString a -> SizedByteString b
 cons = \c b@(SizedByteString fps) -> unsafeDupablePerformIO $ unsafeCreate $ \pd -> do
     poke pd c
     withForeignPtr fps $ \ps -> memcpy (pd `plusPtr` 1) ps $ fromIntegral $ length b
 {-# INLINE cons #-}
 
-snoc :: forall a b. (b ~ (a + 1), NatReflection a, NatReflection b) => SizedByteString a -> Word8 -> SizedByteString b
+snoc :: forall a b. (b ~ (a + 1), SingI a, SingI b) => SizedByteString a -> Word8 -> SizedByteString b
 snoc = \b@(SizedByteString fps) c -> unsafeDupablePerformIO $ unsafeCreate $ \pd -> do
     let size = length b
     withForeignPtr fps $ \ps -> memcpy pd ps $ fromIntegral size
     poke (pd `plusPtr` size) c
 {-# INLINE snoc #-}
 
-head :: forall a. (1 <= a, NatReflection a) => SizedByteString a -> Word8
+head :: forall a. (1 <= a, SingI a) => SizedByteString a -> Word8
 head = \(SizedByteString fp) -> inlinePerformIO $ withForeignPtr fp peek
 {-# INLINE head #-}
 
-last :: forall a. (1 <= a, NatReflection a) => SizedByteString a -> Word8
+last :: forall a. (1 <= a, SingI a) => SizedByteString a -> Word8
 last = \(SizedByteString fp) -> inlinePerformIO $ withForeignPtr fp $ \p -> peekByteOff p off
-  where off = nat (Proxy :: Proxy a) - 1
+  where off = rep (sing :: Sing a) - 1
 {-# INLINE last #-}
